4a746a3ebf32aecb8328495d41070070a1cbf2c2
Roberthall653 <roberthall653@gmail.com>
2023-07-19 08:38:36 -0700

    models updated

--- models/Product.js
+++ models/Product.js
@@ -9,21 +9,31 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    product_id:{
-      type: DataTypes.INTEGER
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
     },
-    product_name:{
-      type: DataTypes.STRING
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     price: {
-      type: DataTypes.DECIMAL[10,2]
+      type: DataTypes.DECIMAL,
+      allowNull: false,
     },
     stock: {
-      type: DataTypes.INTEGER
+      type: DataTypes.INTEGER,
+      allowNull: false,
     },
     category_id: {
-      type: DataTypes.INTEGER
-    },
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+      }
+    }
   },
   {
     sequelize,
